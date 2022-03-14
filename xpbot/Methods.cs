89df4ec7e019fb2e56c1986df2a1fb8e27bbfc65
xbt573 <alunev5@yandex.ru>
2022-03-14 19:16:45 +0300

    changed indentation to spaces

--- xpbot/Methods.cs
+++ xpbot/Methods.cs
@@ -3,44 +3,44 @@
 /// </summary>
 public class Methods
 {
-	// /// <summary>
-	// /// Database class for working with database
-	// /// </summary>
-	// public Database Database { get; set; }
+    // /// <summary>
+    // /// Database class for working with database
+    // /// </summary>
+    // public Database Database { get; set; }
 
-	/// <summary>
-	/// Random class for getting random values
-	/// </summary>
-	public Random rnd { get; set; }
+    /// <summary>
+    /// Random class for getting random values
+    /// </summary>
+    public Random rnd { get; set; }
 
-	public Methods()
-	{
-		// Database = new Database(System.IO.File.ReadAllText("access.txt"));
-		rnd = new Random();
-	}
+    public Methods()
+    {
+        // Database = new Database(System.IO.File.ReadAllText("access.txt"));
+        rnd = new Random();
+    }
 
-	/// <summary>
-	/// Method for getting a token from environment
-	/// </summary>
-	public string? GetToken()
-	{
-		string? token = Environment.GetEnvironmentVariable("TOKEN");
+    /// <summary>
+    /// Method for getting a token from environment
+    /// </summary>
+    public string? GetToken()
+    {
+        string? token = Environment.GetEnvironmentVariable("TOKEN");
 
-		if (token == null) 
-		{
-			return null;
-		}
+        if (token == null) 
+        {
+            return null;
+        }
 
-		return token;
-	}
+        return token;
+    }
 
-	/// <summary>
-	/// Get random value between 1 and 5
-	/// </summary>
-	public int GetRandom()
-	{
-		return rnd.Next(1, 5);
-	}
+    /// <summary>
+    /// Get random value between 1 and 5
+    /// </summary>
+    public int GetRandom()
+    {
+        return rnd.Next(1, 5);
+    }
 
-	// public void AddXp(Update update) {}
+    // public void AddXp(Update update) {}
 }
--- xpbot/Program.cs
+++ xpbot/Program.cs
@@ -7,56 +7,56 @@ using Telegram.Bot.Extensions.Polling;
 /// </summary>
 public class Program
 {
-	/// <summary>
-	/// Main function
-	/// </summary>
-	public static void Main(string[] args)
-	{
-		// Create handlers class
-		Handlers handlers = new Handlers();
+    /// <summary>
+    /// Main function
+    /// </summary>
+    public static void Main(string[] args)
+    {
+        // Create handlers class
+        Handlers handlers = new Handlers();
 
-		// Create methods class
-		Methods methods = new Methods();
+        // Create methods class
+        Methods methods = new Methods();
 
-		// Init logger
-		Log.Logger = new LoggerConfiguration()
-			.MinimumLevel.Debug()
-			.WriteTo.Console()
-			.CreateLogger();
+        // Init logger
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
 
-		Log.Information("Starting program...");
+        Log.Information("Starting program...");
 
-		string? token = methods.GetToken();
+        string? token = methods.GetToken();
 
-		if (token == null) 
-		{
-			Log.Fatal("TOKEN variable not found in environment.");
-			throw new InvalidDataException("TOKEN variable not found in environment.");
-		}
+        if (token == null) 
+        {
+            Log.Fatal("TOKEN variable not found in environment.");
+            throw new InvalidDataException("TOKEN variable not found in environment.");
+        }
 
-		// Init bot
-		TelegramBotClient bot = new TelegramBotClient(token);
+        // Init bot
+        TelegramBotClient bot = new TelegramBotClient(token);
 
-		ReceiverOptions receiverOptions = new ReceiverOptions
-		{
-			AllowedUpdates = { }
-		};
+        ReceiverOptions receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { }
+        };
 
-		CancellationTokenSource cts = new CancellationTokenSource();
+        CancellationTokenSource cts = new CancellationTokenSource();
 
-		bot.StartReceiving(
-			handlers.HandleUpdatesAsync,
-			handlers.HandleErrorsAsync,
-			receiverOptions,
-			cts.Token
-		);
+        bot.StartReceiving(
+            handlers.HandleUpdatesAsync,
+            handlers.HandleErrorsAsync,
+            receiverOptions,
+            cts.Token
+        );
 
-		Log.Information("Bot started!");
+        Log.Information("Bot started!");
 
-		// Infinite loop
-		while (true) 
-		{
-			Thread.Sleep(100);
-		}
-	}
+        // Infinite loop
+        while (true) 
+        {
+            Thread.Sleep(100);
+        }
+    }
 }
